cc54ecfd9bcf6cf5770e2782f420b78823071dba
melvin parra marte <vinck021@hotmail.com>
2020-08-14 23:17:07 -0400

    to new pc

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+   public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public String Name { get; set; }
+    }
+}
