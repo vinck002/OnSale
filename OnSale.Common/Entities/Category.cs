fd30f0cf27c4bf2aa7ee1283c3a9461d00415c80
melvin parra marte <vinck021@hotmail.com>
2020-09-08 12:19:19 -0400

    category

---
+++ OnSale.Common/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener menos de {1} caractereres.")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to put the correct paths
+        [Display(Name = "Imagen")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44390/images/noimage.png"
+            : $"https://onsale.blob.core.windows.net/categories/{ImageId}";
+
+    }
+}
---
+++ OnSale.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnSale.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+
+}
