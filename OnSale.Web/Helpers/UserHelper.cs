467dfedb9debc285cefb1ceefcd5ef77176b1d04
melvin parra marte <vinck021@hotmail.com>
2020-10-08 19:45:43 -0400

    login

--- OnSale.Web/Helpers/UserHelper.cs
+++ OnSale.Web/Helpers/UserHelper.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using OnSale.Web.Data;
 using OnSale.Web.Data.Entities;
+using OnSale.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,14 @@ namespace OnSale.Web.Helpers
         private readonly DataContext _context;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager , SignInManager<User> signInManager)
         {
             _context = context;
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
         }
         public async Task<IdentityResult> AddUserAsync(User user, string password)
         {
@@ -54,6 +57,26 @@ namespace OnSale.Web.Helpers
         {
             return await _userManager.IsInRoleAsync(user, roleName);
         }
+
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await _signInManager.PasswordSignInAsync(
+         model.Username,
+         model.Password,
+         model.RememberMe,
+         false);
+            //El tercer parametro esta false, si lo ponemos true el usuario solo tendra 3 oportunidades de
+
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+
+        }
+
+
     }
 
 }
