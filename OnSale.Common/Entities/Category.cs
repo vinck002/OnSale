cb9137edb4c35a0d500bedf3e8b1f30d9cf4321a
melvin parra marte <vinck021@hotmail.com>
2020-10-16 15:28:22 -0400

    azrure

--- OnSale.Common/Entities/Category.cs
+++ OnSale.Common/Entities/Category.cs
@@ -19,7 +19,7 @@ namespace OnSale.Common.Entities
         //TODO: Pending to put the correct paths
         [Display(Name = "Imagen")]
         public string ImageFullPath => ImageId == Guid.Empty
-            ? $"https://localhost:44313/images/noimage.png"
+            ? $"https://onsaledym.azurewebsites.net/images/noimage.png"
             : $"https://onsale000.blob.core.windows.net/categories/{ImageId}";
         
 
