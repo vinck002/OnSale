d72f755e1a9dd5567d8da25481b0d9cecfad8270
melvin parra marte <vinck021@hotmail.com>
2020-10-16 14:48:05 -0400

    add api token

---
+++ OnSale.Common/Requests/EmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Requests
+{
+   public class EmailRequest
+    {
+
+        [EmailAddress]
+        [Required]
+        public String Email { get; set; }
+    }
+}
